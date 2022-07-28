7b648ade6e47c2c72578c6eec143c684cd9914b3
Piyush Dhall <contact@piyush.ovh>
2022-07-29 03:24:02 +0530

    video player server. added plyr for frontend

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const fs = require("fs");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.get("/video", (req, res) => {
+  const range = req.headers.range;
+  if (!range) {
+    res.status(400).send("Requires range header");
+  }
+
+  const videoPath = "video.mp4";
+  const videoSize = fs.statSync(videoPath).size;
+
+  // Parse Range
+  // Example : "bytes=32324-"
+
+  const CHUNK_SIZE = 10 ** 6; // 1MB
+  const start = Number(range.replace(/\D/g, ""));
+  const end = Math.min(start + CHUNK_SIZE, videoSize - 1);
+
+  const contentLength = end - start + 1;
+  const headers = {
+    "Content-Range": `bytes ${start}-${end}/${videoSize}`,
+    "Accept-Ranges": "bytes",
+    "Content-Lenth": contentLength,
+    "Content-Type": "video/mp4",
+  };
+
+  res.writeHead(206, headers);
+
+  const videoStream = fs.createReadStream(videoPath, { start, end });
+
+  videoStream.pipe(res);
+});
+
+app.listen(8000, () => {
+  console.log("Listening on port 8000!");
+});
